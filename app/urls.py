7c7f94baab03a1646fbd412dacde44292a9ae206
crackoscl <gilbert.ata@gmail.com>
2021-09-08 22:32:46 -0300

    se agrega al path /hoteles

--- app/urls.py
+++ app/urls.py
@@ -5,7 +5,7 @@ app_name = 'app'
 
 urlpatterns = [
     path('', Principal.as_view(), name='principal'),
-    path('<slug:slug>/', Detalle.as_view(), name='detalle_hotel'),
-    path('reviews/<pk>', Review.as_view(), name='reviews'),
-    path('filter/<pk>', filter, name='filter')
+    path('hoteles/<slug:slug>/', Detalle.as_view(), name='detalle_hotel'),
+    path('hoteles/reviews/<pk>', Review.as_view(), name='reviews'),
+    path('hoteles/filter/<pk>', filter, name='filter')
 ]
